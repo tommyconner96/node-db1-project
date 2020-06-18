bc92eac1bfab285b2a3b8eb0038730b2fe429d05
unknown <tommyconner96@gmail.com>
2020-06-17 21:09:59 -0400

    express setup, error handling setup,start script setup, account router setup but no CRUD yet

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const server = require("./api/server.js");
+const express = require("express")
+const server = require("./api/server.js")
+const accountsRouter = require("./api/accounts/accounts-router")
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
+//express
+server.use(express.json())
+//route
+server.use("/api/accounts", accountsRouter)
+//error handling
+server.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).json({
+    message: "Something went wrong, please try again later",
+  })
+})
 
 server.listen(PORT, () => {
-  console.log(`\n== API running on port ${PORT} ==\n`);
-});
+  console.log(`\n== API running on port ${PORT} ==\n`)
+})
